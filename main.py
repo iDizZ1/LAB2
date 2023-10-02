808bad4270763ad9e5fa451da82e77915fde0ae5
iDizZ1 <idizz100@gmail.com>
2023-10-02 14:49:18 +0300

    2

--- main.py
+++ main.py
@@ -11,5 +11,7 @@ def calculator(num1: int, num2: int, op: str):
     if op == "*":
         return {num1 * num2}
     if op == "/":
+        if num2 == 0:
+            return {"Нельзя делить на 0"}
         return {num1 / num2}
     return {"Ошибка"}
